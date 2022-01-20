48e963e696f5fa134646eef4c931f85c5622a131
zicchi <zidanezicchi@gmail.com>
2022-01-20 12:43:16 +0700

    update view

--- resources/views/pages/admin/users/index.blade.php
+++ resources/views/pages/admin/users/index.blade.php
@@ -22,25 +22,25 @@
                             <table class="table table-striped table-md">
                                 <tr>
                                     <th>Name</th>
-                                    <th>Poin</th>
                                     <th>Status</th>
                                     <th>Action</th>
                                 </tr>
                                 @foreach($users as $user)
                                         <tr>
                                             <td>{{$user->name}}</td>
-                                            <td>{{$user->point}}</td>
                                             <td>
                                                 @if($user->attended == false)
-                                                    <span class="badge badge-danger">Belum Absen</span>
+                                                    <span class="badge badge-danger">Belum ambil hadiah</span>
                                                 @else
-                                                    <span class="badge badge-success">Sudah Absen</span>
+                                                    <span class="badge badge-success">Sudah ambil hadiah</span>
                                                 @endif
                                             </td>
                                             <td>
                                                 <a href="{{route('admin::users::view',[hashid_encode($user->id,'user')])}}" class="btn btn-link">Rincian</a>
                                                 @if($user->attended == false)
-                                                    <a href="{{route('api::index',[$user])}}" target="_blank" class="btn btn-primary">Absen</a>
+                                                    <a href="javascript:" onclick="if(confirm('Hadiah {{$user->name}} akan diambil')){$('#get-item-{{$user->id}}').submit()}" class="btn btn-primary">Hadiah</a>
+                                                    <form target="_blank" action="{{ route('api::index', [$user]) }}" method="get" class="hidden" id="get-item-{{ $user->id }}">
+                                                    </form>
                                                 @endif
                                             </td>
                                         </tr>
