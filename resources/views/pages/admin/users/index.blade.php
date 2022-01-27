1091292bd8de30256e1ffc51a2591d504878a4c4
Mochammad Fawwaz Islami <mailmcfawwaz@gmail.com>
2022-01-27 10:01:27 +0700

    add table responsive

--- resources/views/pages/admin/users/index.blade.php
+++ resources/views/pages/admin/users/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.main')
 @section('title')
-    {{ $user->name }}
+    Data Undangan
 @endsection
 @section('content')
     <section class="section">
@@ -8,43 +8,75 @@
             <h1>@yield('title')</h1>
             <div class="section-header-breadcrumb">
                 <div class="breadcrumb-item active"><a href="{{ route('admin::dashboard') }}">Dashboard</a></div>
-                <div class="breadcrumb-item active"><a href="{{ route('admin::users::index') }}">User</a></div>
-                <div class="breadcrumb-item">{{ $user->name }}</div>
+                <div class="breadcrumb-item">@yield('title')</div>
             </div>
         </div>
 
         <div class="section-body">
-
-            <div class="btn-lg my-2">
-                <a href="{{ route('admin::users::edit', [$user]) }}" class="btn btn-info">Update</a>
-                <a href="javascript:"
-                    onclick="if(confirm('Anda yakin ingin menghapus item ini?')){$('#delete-item-{{ $user->id }}').submit()};"
-                    class="btn btn-danger">Hapus</a>
-                <form action="{{ route('admin::users::destroy', [$user]) }}" method="post" class="hidden"
-                    id="delete-item-{{ $user->id }}">
-                    @csrf
-                    @method('delete')
-                </form>
-            </div>
-
-            <div class="row">
-                <div class="col-md-6">
-                    <div class="card">
-                        <div class="card-body">
-                            <dt>ID</dt>
-                            <dd>#{{ $user->id }}</dd>
-                            <dt>Nama</dt>
-                            <dd>{{ $user->name }}</dd>
-                        </div>
+            <div class="card">
+                <div class="card-header">
+                    <a href="{{ route('admin::users::create') }}" class="btn btn-primary mr-auto">Tambah User +</a>
+                    <div class="card-header-action">
+                        <form action="{{ route('admin::users::index') }}">
+                            <div class="input-group">
+                                <input type="text" name="name" class="form-control" placeholder="Search">
+                                <div class="input-group-btn">
+                                    <button class="btn btn-primary text-white">Cari</button>
+                                </div>
+                            </div>
+                        </form>
                     </div>
                 </div>
-                {{-- <div class="col-md-6"> --}}
-                {{-- <div class="card"> --}}
-                {{-- <div class="card-body"> --}}
-                {{-- <img alt="" src="data:image/png;base64, {!! base64_encode(SimpleSoftwareIO\QrCode\Facades\QrCode::format('png')->size(300)->generate(route('api::index', [hashid_encode($user->id,'user')]))) !!} "> --}}
-                {{-- </div> --}}
-                {{-- </div> --}}
-                {{-- </div> --}}
+                <div class="card-body p-0">
+                    <div class="table-responsive">
+                        <table class="table table-bordered">
+                            <tr>
+                                <th>No</th>
+                                <th>Name</th>
+                                <th>Jabatan</th>
+                                <th>Fakultas</th>
+                                <th>Merchandise</th>
+                                <th>Action</th>
+                            </tr>
+                            @foreach ($users as $user)
+                                <tr>
+                                    <td>{{ $user->id }}</td>
+                                    <td>{{ $user->name }}</td>
+                                    <td>{{ $user->jabatan }}</td>
+                                    <td>{{ $user->fakultas }}</td>
+                                    <td>
+                                        @if ($user->status == 100)
+                                            <span class="badge badge-danger">Belum Ambil</span>
+                                        @elseif($user->status == 1)
+                                            <span class="badge badge-warning">Pending</span>
+                                        @else
+                                            <span class="badge badge-success">Sudah Ambil</span>
+                                        @endif
+                                    </td>
+                                    <td>
+                                        <a href="{{ route('admin::users::view', [hashid_encode($user->id, 'user')]) }}"
+                                            class="btn btn-link">Rincian</a>
+                                        @if ($user->status == \App\Models\User::STATUS_DEFAULT)
+                                            <a href="javascript:"
+                                                onclick="if(confirm('Hadiah {{ $user->name }} akan diambil')){$('#get-item-{{ $user->id }}').submit()}"
+                                                class="btn btn-primary">Konfirmasi</a>
+                                            <form action="{{ route('api::index', [$user]) }}" method="get"
+                                                class="hidden" id="get-item-{{ $user->id }}">
+                                            </form>
+                                        @endif
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </table>
+                    </div>
+                </div>
+                <div class="card-footer text-right">
+                    <nav class="d-inline-block">
+                        <ul class="pagination mb-0">
+                            {{ $users->links() }}
+                        </ul>
+                    </nav>
+                </div>
             </div>
         </div>
     </section>
--- resources/views/pages/main/user/index.blade.php
+++ resources/views/pages/main/user/index.blade.php
@@ -26,41 +26,43 @@
                     </div>
                 </div>
                 <div class="card-body p-0">
-                    <table class="table table-bordered">
-                        <tr>
-                            <th scope="col">No</th>
-                            <th scope="col">Nama</th>
-                            <th scope="col">Jabatan</th>
-                            <th scope="col">Fakultas</th>
-                            <th scope="col">Merchandise</th>
-                            <th scope="col">Action</th>
-                        </tr>
-                        @foreach ($users as $user)
+                    <div class="table-responsive">
+                        <table class="table table-bordered">
                             <tr>
-                                <td scope="row">{{ $user->id }}</td>
-                                <td>{{ $user->name }}</td>
-                                <td>{{ $user->jabatan }}</td>
-                                <td>{{ $user->fakultas }}</td>
-                                <td>
-                                    @if ($user->status == 100)
-                                        <span class="badge badge-danger">Belum Diambil</span>
-                                    @else
-                                        <span class="badge badge-success">Sudah Diambil</span>
-
-                                    @endif
-                                </td>
-                                <td>
-                                    @if ($user->status == 100)
-                                        <button class="btn btn-primary" data-toggle="modal"
-                                            data-target="#exampleModal{{ $user->id }}">
-                                            Konfirmasi</button>
-                                    @else
-                                        <button class="btn btn-secondary">Konfirmasi</button>
-                                    @endif
-                                </td>
+                                <th scope="col">No</th>
+                                <th scope="col">Nama</th>
+                                <th scope="col">Jabatan</th>
+                                <th scope="col">Fakultas</th>
+                                <th scope="col">Merchandise</th>
+                                <th scope="col">Action</th>
                             </tr>
-                        @endforeach
-                    </table>
+                            @foreach ($users as $user)
+                                <tr>
+                                    <td scope="row">{{ $user->id }}</td>
+                                    <td>{{ $user->name }}</td>
+                                    <td>{{ $user->jabatan }}</td>
+                                    <td>{{ $user->fakultas }}</td>
+                                    <td>
+                                        @if ($user->status == 100)
+                                            <span class="badge badge-danger">Belum Diambil</span>
+                                        @else
+                                            <span class="badge badge-success">Sudah Diambil</span>
+
+                                        @endif
+                                    </td>
+                                    <td>
+                                        @if ($user->status == 100)
+                                            <button class="btn btn-primary" data-toggle="modal"
+                                                data-target="#exampleModal{{ $user->id }}">
+                                                Konfirmasi</button>
+                                        @else
+                                            <button class="btn btn-secondary">Konfirmasi</button>
+                                        @endif
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </table>
+                    </div>
                 </div>
             </div>
             <div class="card-footer text-right mb-8">
